3ac437126077b4f27a3edda981d239d9c61afcb8
Sayanta <sayanta2702@gmail.com>
2020-10-10 18:43:34 +0530

    Production Build

---
+++ precache-manifest.fe240cf2742bc34b02dbc7b4bc453804.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac65ae75f3f626a2ee75ba17054d46d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb567f8e604dbf54da02",
+    "url": "/static/css/main.cafd0e6c.chunk.css"
+  },
+  {
+    "revision": "d1034490c34cf24058ff",
+    "url": "/static/js/2.aed87ffa.chunk.js"
+  },
+  {
+    "revision": "92208c9e2611b774f131c490ad0eb0b5",
+    "url": "/static/js/2.aed87ffa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb567f8e604dbf54da02",
+    "url": "/static/js/main.695a695c.chunk.js"
+  },
+  {
+    "revision": "537147f468f02cd086f3",
+    "url": "/static/js/runtime-main.01beb74d.js"
+  },
+  {
+    "revision": "04260f044e9ae148b318ec7fe36fea27",
+    "url": "/static/media/404.04260f04.svg"
+  }
+]);
