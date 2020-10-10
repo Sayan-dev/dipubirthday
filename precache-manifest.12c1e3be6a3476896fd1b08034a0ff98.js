5be54787198470661f2d124f53c026cbf753d098
Sayanta <sayanta2702@gmail.com>
2020-10-10 18:54:25 +0530

    Updates

---
+++ precache-manifest.12c1e3be6a3476896fd1b08034a0ff98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5b8f9c26cbea2c899c82ce76d804844",
+    "url": "/dipubirthday/index.html"
+  },
+  {
+    "revision": "eb567f8e604dbf54da02",
+    "url": "/dipubirthday/static/css/main.cafd0e6c.chunk.css"
+  },
+  {
+    "revision": "d1034490c34cf24058ff",
+    "url": "/dipubirthday/static/js/2.aed87ffa.chunk.js"
+  },
+  {
+    "revision": "92208c9e2611b774f131c490ad0eb0b5",
+    "url": "/dipubirthday/static/js/2.aed87ffa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb567f8e604dbf54da02",
+    "url": "/dipubirthday/static/js/main.695a695c.chunk.js"
+  },
+  {
+    "revision": "ccfc3b0f26e0622294bc",
+    "url": "/dipubirthday/static/js/runtime-main.41383060.js"
+  },
+  {
+    "revision": "04260f044e9ae148b318ec7fe36fea27",
+    "url": "/dipubirthday/static/media/404.04260f04.svg"
+  }
+]);
